9f8cf80527daa569043e79eaf51987c42f795beb
tamil <tamil.accenture@gmail.com>
2025-02-09 01:00:05 -0500

    Summarize.py Execute Four Value counts as Numeric

--- Pandas Basics/Summarize Data/summarize.py
+++ Pandas Basics/Summarize Data/summarize.py
@@ -1,3 +1,29 @@
+# Execute One, Two, Three
+# import pandas as pd
+# from dataclasses import dataclass
+
+# @dataclass
+# class Summarize:
+#     filename : str
+    
+#     def panadasDataFrame(self):
+#         return pd.read_csv(self.filename)
+
+
+# washers = Summarize("washers.csv")
+# washers_df = washers.panadasDataFrame()
+# print(washers_df.head())
+
+# print(washers_df.info())
+
+# print("********** String Data ***********")
+# print(washers_df[['BrandName']].describe())
+# print("**********Numeric Data************")
+# print(washers_df[['Volume']].describe())
+
+
+# Execute Four
+
 import pandas as pd
 from dataclasses import dataclass
 
@@ -11,9 +37,6 @@ class Summarize:
 
 washers = Summarize("washers.csv")
 washers_df = washers.panadasDataFrame()
-# print(washers_df.head())
-# print(washers_df.info())
-print("********** String Data ***********")
-print(washers_df[['BrandName']].describe())
-print("**********Numeric Data************")
-print(washers_df[['Volume']].describe())
+
+print("********** Value Counts ***********")
+print(washers_df[['BrandName']].value_counts())
