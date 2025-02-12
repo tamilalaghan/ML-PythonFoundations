d703a9ceabf5dcad079c8f753be5efa57e1f4a3e
tamil <tamil.accenture@gmail.com>
2025-02-12 13:23:52 -0500

    Missing Values - fillna Execute Two

--- Filling Missing Values/Fillna/README.md
+++ Filling Missing Values/Fillna/README.md
@@ -24,9 +24,44 @@ mask = student_df['State'].isnull()
 print("*********Missing States************")
 print(student_df[mask])
 
-print("*********Filled Missing States*cl********")
+print("*********Filled Missing States*********")
 print(student_df[mask].fillna({"State":"NY"}))
 ```
 
 Outcome
 ![alt text](image.png)
+
+Execute Two
+
+```py
+# Execute Two
+import pandas as pd
+from dataclasses import dataclass
+
+@dataclass
+class StudentsData:
+      filename :str
+
+      def toPandasDataFrame(self):
+            return pd.read_excel(self.filename)
+      
+
+dataset_filename = "students.xlsx"
+
+students = StudentsData(dataset_filename)
+student_df = students.toPandasDataFrame().copy()
+
+print("*********Before Dropping***********")
+print(student_df)
+
+mask = student_df['Age'].isnull()
+print("*********Missing States************")
+print(student_df[mask])
+
+print("*********Filled Missing Age*********")
+print(student_df[mask].fillna({"Age": 
+      round(student_df["Age"].median()) }))
+```
+
+Outcome
+![alt text](image-1.png)
--- Filling Missing Values/Fillna/fillna.py
+++ Filling Missing Values/Fillna/fillna.py
@@ -1,4 +1,32 @@
+# # Execute One
+# import pandas as pd
+# from dataclasses import dataclass
 
+# @dataclass
+# class StudentsData:
+#       filename :str
+
+#       def toPandasDataFrame(self):
+#             return pd.read_excel(self.filename)
+      
+
+# dataset_filename = "students.xlsx"
+
+# students = StudentsData(dataset_filename)
+# student_df = students.toPandasDataFrame().copy()
+
+# print("*********Before Dropping***********")
+# print(student_df)
+
+# mask = student_df['State'].isnull()
+# print("*********Missing States************")
+# print(student_df[mask])
+
+# print("*********Filled Missing States*cl********")
+# print(student_df[mask].fillna({"State":"NY"}))
+
+
+# Execute Two
 import pandas as pd
 from dataclasses import dataclass
 
@@ -18,9 +46,10 @@ student_df = students.toPandasDataFrame().copy()
 print("*********Before Dropping***********")
 print(student_df)
 
-mask = student_df['State'].isnull()
+mask = student_df['Age'].isnull()
 print("*********Missing States************")
 print(student_df[mask])
 
-print("*********Filled Missing States*cl********")
-print(student_df[mask].fillna({"State":"NY"}))
+print("*********Filled Missing Age*********")
+print(student_df[mask].fillna({"Age": 
+      round(student_df["Age"].median()) }))
