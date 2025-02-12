c8dfda878db393e53f3b188db5dda20b2de89532
tamil <tamil.accenture@gmail.com>
2025-02-12 13:02:24 -0500

    Missing Values - fillna Execute One

---
+++ Filling Missing Values/Fillna/fillna.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+from dataclasses import dataclass
+
+@dataclass
+class StudentsData:
+      filename :str
+
+      def toPandasDataFrame(self):
+            return pd.read_excel(self.filename)
+      
+
+dataset_filename = "students.xlsx"
+
+students = StudentsData(dataset_filename)
+student_df = students.toPandasDataFrame().copy()
+
+print("*********Before Dropping***********")
+print(student_df)
+
+mask = student_df['State'].isnull()
+print("*********Missing States************")
+print(student_df[mask])
+
+print("*********Filled Missing States*cl********")
+print(student_df[mask].fillna({"State":"NY"}))
