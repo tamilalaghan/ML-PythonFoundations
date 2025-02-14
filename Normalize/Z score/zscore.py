cde4744e6d5f4dd0aa75c883488f75b875077e73
tamil <tamil.accenture@gmail.com>
2025-02-13 23:57:05 -0500

    Normalize ZScore One

---
+++ Normalize/Z score/zscore.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+from dataclasses import dataclass
+import matplotlib.pyplot as plt
+
+
+@dataclass
+class VehicleDataFrame:
+    inputfilename :str
+
+    def toPandasDataFrame(self):
+        return pd.read_csv(self.inputfilename)
+    
+
+filename = "vehicles.csv"
+
+vehicle = VehicleDataFrame(filename)
+vehicle_df = vehicle.toPandasDataFrame()
+
+print(vehicle_df['co2emissions'].describe())
+
+vehicle_df[['co2emissions']].plot(kind = "hist")
+
+plt.title("Before ZScore Normalization")
+
+plt.show()
